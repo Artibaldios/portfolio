b80b57793eaba82be6ad91621968099f07221b02
ArtyomBorisevich <aborisevich@xbsoftware.by>
2025-08-13 17:27:06 +0300

    [dev] deploy fixies 4.0

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
-const nextConfig : NextConfig = {
-  reactStrictMode: true,
-  basePath: '/portfolio',
-  output: 'export', // Export as static HTML
+const nextConfig: NextConfig = {
+  output: 'export', // Static HTML export mode
+  basePath: '/portfolio', // Replace with your repo name (critical for GitHub Pages)
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  // Optional: Set the output folder (default is 'out')
+  distDir: 'out',
 };
 
 export default nextConfig;
